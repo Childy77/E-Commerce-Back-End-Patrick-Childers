faf5416b904cd04c365819bbcd5400fe9084dcfa
Ryan <ryanbairdchilders@gmail.com>
2023-02-13 12:57:58 -0800

    seeded database

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findBypkg(req.params.id, {
+    const tagData = await Tag.findBypk(req.params.id, {
       include: [
         {model: Product}
       ],
